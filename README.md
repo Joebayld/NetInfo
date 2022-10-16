8bd96e5a2c13fb095f8579810119e2c4b2d47311
Joe Bay <Joebayld@gmail.com>
2022-10-15 18:50:21 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NetInfo
+
+A description of this package.
---
+++ Tests/NetInfoTests/NetInfoTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NetInfo
+
+final class NetInfoTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NetInfo().text, "Hello, World!")
+    }
+}
